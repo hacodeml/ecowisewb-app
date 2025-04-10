1258dfba276a0bddac2b4bc5159a07a5c28f5f8f
Harold Mantilla <46107517+hacodeml@users.noreply.github.com>
2025-04-10 11:07:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className=\"text-center text-white min-h-screen bg-blue-700 flex items-center justify-center\">
-      <h1 className=\"text-4xl font-bold\">Bienvenido a EcoWise Web</h1>
+    <div className="text-center text-white min-h-screen bg-blue-700 flex items-center justify-center\">
+      <h1 className="text-4xl font-bold\">Bienvenido a EcoWise Web</h1>
     </div>
   );
 }
